53eab08983974b958fc02a4d167d90b40a6a14fe
DamianRondinon <damianrondinon@gmail.com>
2024-03-02 23:52:21 -0300

    syntax arrangements

--- components/works/Education1.tsx
+++ components/works/Education1.tsx
@@ -1,5 +1,5 @@
-import { motion } from 'framer-motion';
-import { FaAngleDoubleRight } from 'react-icons/fa';
+import { motion } from "framer-motion";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
 const Education1 = () => {
   return (
